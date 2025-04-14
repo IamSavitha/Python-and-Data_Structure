83a7f7aacc6025b0e854fa4dd354e284acd43748
IamSavitha <savitha.vijayarangan09@gmail.com>
2025-04-13 23:18:20 -0700

    Steps to follow added inn readme

--- Lab2/app.py
+++ Lab2/app.py
@@ -2,7 +2,6 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas_datareader as data 
 from keras.models import load_model
 import streamlit as st 
 from sklearn.preprocessing import MinMaxScaler
